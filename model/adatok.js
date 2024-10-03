adec35e1d8ffdf7947ed9d0a728c13b8ba01905b
minaii93 <hajnalkaminaei@gmail.com>
2024-10-03 19:38:59 +0200

    adatok

---
+++ model/adatok.js
@@ -0,0 +1,22 @@
+export const termekLista = [
+    {
+        elnevezes: "Cappuccino",
+        leiras : "Klasszikus olasz kávé tejhabbal",
+        ar: 4
+    },
+    {
+        elnevezes: "Latte Macchiato",
+        leiras : "Krémes tejjel készült eszpresszó",
+        ar: 4
+    },
+    {
+        elnevezes: "Americano",
+        leiras : "Víz hozzáadásával készült eszpresszó",
+        ar: 4
+    },
+    {
+        elnevezes: "Flat White",
+        leiras : "Dupla eszpresszó tejhabbal",
+        ar: 5
+    }
+]
